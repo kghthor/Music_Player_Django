0894679584915d0e9ac3f1a9284c185ee6188849
Harish K G 💻🚀 <86233829+kghthor@users.noreply.github.com>
2024-06-27 16:02:37 +0530

    Update admin.py

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -3,7 +3,7 @@ from .models import *
 
 # Register your models here.
  
-admin.site.register(Song)
+admin.site.register(Song) 
 admin.site.register(Playlist)
 admin.site.register(Favourite)
 admin.site.register(Recent)
